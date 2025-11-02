a3fcac236420640a070bc14cd58a7766938d3720
Wasana Udayangani <udayangani97nibm@gmail.com>
2025-11-02 20:05:39 +0530

    deleted my finance old folder

--- my-finance/src/main/java/com/my_finance/my_finance/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.my_finance.my_finance.Repository;
-
-import com.my_finance.my_finance.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User,Integer> {
-
-}
